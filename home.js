5f1f63056cf36766ab5642ce82b126db8343233f
merrickt <39343323+merrickt@users.noreply.github.com>
2020-02-16 09:30:21 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,16 +1,24 @@
 function search() {
 	var input = document.getElementById("search").value;
 	const coursesAPI = "https://api.umd.io/v0/courses?";
+	const linkBase = "http://jhacks2020.glitch.me/classes/";
+	const classRegExp = /([A-Z]{4})([0-9]{3})/;
 	const ul = document.getElementById("class_list");
 	var str = "";
 
-	fetch(coursesAPI + "dept_id=" + input).then((response)=> { 
+	fetch(coursesAPI + "dept_id=" + input).then((response)=> {
 		return response.json();
 	}).then((myJson) => { for (i = 0; i < myJson.length; i++) {
-					str += "<li>" + myJson[i].course_id + "</li>";
+
+					match = myJson[i].course_id.match(classRegExp);
+					link = linkBase + match[1] + "/" + match[2];
+
+					str += "<li><a class=\"btn btn-primary\" href=link role=\"button\">"
+									+ myJson[i].course_id + "</a></li>";
+
+
+									
 				}
 				ul.innerHTML = str;
 	});
-}	
-
-
+}
